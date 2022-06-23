ce600f64ca80e0374fd2451bdd1550490d18e7a4
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-23 11:46:21 +0000

    [FInish]4-10,[Start]4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,15 @@
 class PostImagesController < ApplicationController
+
   def new
+    @post_image = PostImage.new
+  end
+
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -7,4 +17,12 @@ class PostImagesController < ApplicationController
 
   def show
   end
+
+  # ストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
   get 'homes/about', as: 'about'
 end
