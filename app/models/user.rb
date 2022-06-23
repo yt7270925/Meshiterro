1f9392be00629c0661705167729979bc53553d84
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-23 13:37:57 +0000

    [Finish]4-15,[Start]4-16

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
   # ActiveStorageの設定
   has_one_attached :profile_image
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [width, height]).processed #画像サイズの変更
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
+  profile_image.variant(resize_to_limit: [width, height]).processed #画像サイズの変更
+end
 
 end
