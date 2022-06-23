b8dde972e386e202a32ea01fef28c1de5ebde9aa
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-23 13:00:55 +0000

    [Finish]4-13,[Start]4-14

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,7 +9,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path
+    redirect_to post_images_path #indexへのリダイレクト
   end
 
   def index
@@ -17,8 +17,13 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    # @post_image = PostImage.find(:id)
     @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path #indexへのリダイレクト
   end
 
   # ストロングパラメータ
